8f2b5df4ed45d5a3846218d378f04ed5c850213b
Phuchit Sirimongkolsathien <phuchit.sir@ascendcorp.com>
2017-10-13 14:00:48 +0700

    Init polling price cryptocurrency

---
+++ index.js
@@ -0,0 +1,33 @@
+const fetch = require('isomorphic-unfetch')
+const ENDPOINT_BX = 'https://bx.in.th/api/'
+const ENDPOINT_COIN_MARKET_CAP = 'https://api.coinmarketcap.com/v1/ticker/'
+const BX_KEY_OMG = '26'
+const BX_KEY_EVX = '28'
+const CMC_KEY_OMG = 'omisego'
+const CMC_KEY_EVX = 'everex'
+
+const fetchBx = async () => {
+    let response = await fetch(ENDPOINT_BX)
+    let bx = await response.json()
+    return {
+        omg: bx[BX_KEY_OMG].last_price,
+        evx: bx[BX_KEY_EVX].last_price
+    }
+}
+
+const fetchCoinmarketCap = async () => {
+    let response = await fetch(ENDPOINT_COIN_MARKET_CAP)
+    let cmc = await response.json()
+    let [omg, evx] = await cmc.filter(v => v.id == CMC_KEY_OMG || v.id == CMC_KEY_EVX).map(v => v.price_usd)
+    return {
+        omg,
+        evx
+    }
+}
+
+const fetchAll = async () => {
+    let response = await Promise.all([fetchBx(), fetchCoinmarketCap()])
+    console.log(response)
+}
+
+setInterval(fetchAll, 5000)
