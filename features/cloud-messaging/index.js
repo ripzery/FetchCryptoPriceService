af15a51a24d314c20f6a2d7468575b1e7cb289fd
Phuchit Sirimongkolsathien <ripzery@gmail.com>
2017-10-14 22:14:05 +0700

    Refactor cloud messaging module

--- features/cloud-messaging/index.js
+++ features/cloud-messaging/index.js
@@ -4,38 +4,46 @@ const COLOR_RED = '#f44336'
 const COLOR_GREEN = '#4caf50'
 
 class CloudMessagingService {
+    constructor() {
+        /* Build payload based on price is going up or down */
+        this.payload = {
+            data: {
+                currentPrice: '',
+                type: '',
+                title: "Cryptracker",
+                body: '',
+                color: ''
+            }
+        }
+        this.messaging = admin.messaging()
+    }
+
     async notifyUsers(waitingNotifyUsers, price) {
         let waitingNotifyUserTokens = {
             priceUp: waitingNotifyUsers.priceUp.map(v => v.refreshedToken),
             priceDown: waitingNotifyUsers.priceDown.map(v => v.refreshedToken)
         }
-    
+
+        /* Update current price */
+        this.payload.data.currentPrice = `${price.omg}`
+
         /* If there're no users should be notified then do nothing. */
         if (waitingNotifyUserTokens.priceUp.length + waitingNotifyUserTokens.priceDown.length == 0) return
-    
-        /* Build payload based on price is going up or down */
-        let payload = {
-            data: {
-                currentPrice: `${price.omg}`,
-                type: "",
-                title: "Cryptracker"
-            }
-        }
-    
+
         if (waitingNotifyUserTokens.priceUp.length) {
-            payload.data.type = "up"
-            payload.data.body = `Hooray! OMG is going up more than 5%!. The current price is now ${price.omg}`
-            payload.data.color = COLOR_GREEN
-            let response = await admin.messaging().sendToDevice(waitingNotifyUserTokens.priceUp, payload)
+            this.payload.data.type = "up"
+            this.payload.data.body = `Hooray! OMG is going up more than 5%!. The current price is now ${price.omg}`
+            this.payload.data.color = COLOR_GREEN
+            let response = await this.messaging.sendToDevice(waitingNotifyUserTokens.priceUp, this.payload)
             this.logFCMResponse(response)
             FirestoreService.updateDocument(!response.failureCount, [...waitingNotifyUsers.priceUp, ...waitingNotifyUsers.priceDown], price)
         }
-    
+
         if (waitingNotifyUserTokens.priceDown.length) {
-            payload.data.type = "down"
-            payload.data.body = `Boo.. OMG is going down 5% check it out. The current price is now ${price.omg}`
-            payload.data.color = COLOR_RED
-            let response = await admin.messaging().sendToDevice(waitingNotifyUserTokens.priceDown, payload)
+            this.payload.data.type = "down"
+            this.payload.data.body = `Boo.. OMG is going down 5% check it out. The current price is now ${price.omg}`
+            this.payload.data.color = COLOR_RED
+            let response = await this.messaging.sendToDevice(waitingNotifyUserTokens.priceDown, this.payload)
             this.logFCMResponse(response)
             FirestoreService.updateDocument(!response.failureCount, [...waitingNotifyUsers.priceUp, ...waitingNotifyUsers.priceDown], price)
         }
--- features/market/index.js
+++ features/market/index.js
@@ -28,7 +28,6 @@ class MarketService {
             evx
         }
     }
-
 }
 
 module.exports = new MarketService()
