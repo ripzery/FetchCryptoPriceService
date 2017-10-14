226f4ba7be11be78ceed87e4525ac1d19f9cce73
Phuchit Sirimongkolsathien <ripzery@gmail.com>
2017-10-14 22:08:05 +0700

    Refactor fetching price to module market

---
+++ features/market/index.js
@@ -0,0 +1,34 @@
+class MarketService {
+    constructor() {
+        this.ENDPOINT_BX = 'https://bx.in.th/api/'
+        this.ENDPOINT_COIN_MARKET_CAP = 'https://api.coinmarketcap.com/v1/ticker/'
+        this.BX_KEY_OMG = '26'
+        this.BX_KEY_EVX = '28'
+        this.CMC_KEY_OMG = 'omisego'
+        this.CMC_KEY_EVX = 'everex'
+    }
+
+    /* Fetch Bx.in.th price */
+    async fetchBx() {
+        let response = await fetch(this.ENDPOINT_BX)
+        let bx = await response.json()
+        return {
+            omg: bx[this.BX_KEY_OMG].last_price,
+            evx: bx[this.BX_KEY_EVX].last_price
+        }
+    }
+
+    /* Fetch coinmarketcap.com price */
+    async fetchCoinmarketCap() {
+        let response = await fetch(this.ENDPOINT_COIN_MARKET_CAP)
+        let cmc = await response.json()
+        let [omg, evx] = await cmc.filter(v => v.id == this.CMC_KEY_OMG || v.id == this.CMC_KEY_EVX).map(v => v.price_usd)
+        return {
+            omg,
+            evx
+        }
+    }
+
+}
+
+module.exports = new MarketService()
--- index.js
+++ index.js
@@ -1,42 +1,15 @@
 const fetch = require('isomorphic-unfetch')
 const FirestoreService = require('./features/firestore')
 const CloudMessagingService = require('./features/cloud-messaging')
-
-const ENDPOINT_BX = 'https://bx.in.th/api/'
-const ENDPOINT_COIN_MARKET_CAP = 'https://api.coinmarketcap.com/v1/ticker/'
-const BX_KEY_OMG = '26'
-const BX_KEY_EVX = '28'
-const CMC_KEY_OMG = 'omisego'
-const CMC_KEY_EVX = 'everex'
-
+const MarketService = require('./features/market')
 const PRICE_DEVIATION = 5 / 100; // Notify when price change (up/down) over x %
 
-/* Fetch Bx.in.th price */
-const fetchBx = async () => {
-    let response = await fetch(ENDPOINT_BX)
-    let bx = await response.json()
-    return {
-        omg: bx[BX_KEY_OMG].last_price,
-        evx: bx[BX_KEY_EVX].last_price
-    }
-}
-
-/* Fetch coinmarketcap.com price */
-const fetchCoinmarketCap = async () => {
-    let response = await fetch(ENDPOINT_COIN_MARKET_CAP)
-    let cmc = await response.json()
-    let [omg, evx] = await cmc.filter(v => v.id == CMC_KEY_OMG || v.id == CMC_KEY_EVX).map(v => v.price_usd)
-    return {
-        omg,
-        evx
-    }
-}
-
-/* Combine all together */
+/* Combine all services together */
 const process = async () => {
-    let [bxPrice, cmcPrice] = await Promise.all([fetchBx(), fetchCoinmarketCap()])
+    let [bxPrice, cmcPrice] = await Promise.all([MarketService.fetchBx(), MarketService.fetchCoinmarketCap()])
     let waitingNotifyUsers = await FirestoreService.fetchNeededNotifyUsers(bxPrice, PRICE_DEVIATION)
-    CloudMessagingService.notifyUsers(waitingNotifyUsers, bxPrice)
+    let result = await CloudMessagingService.notifyUsers(waitingNotifyUsers, bxPrice)
+    return result
 }
 
 process()
